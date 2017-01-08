bb91e9bf3ce69b2058137fb7a544e1881e572d82
Marko Zub <mzub@n-ix.com>
2017-01-08 17:03:12 +0200

    hardcoded user login

--- public/src/app/index.interceptors.js
+++ public/src/app/index.interceptors.js
@@ -18,6 +18,9 @@
         if (rejection.status === -1) {
           var MsgService = $injector.get('MsgService');
           MsgService.error('Server error');
+        } else if(rejection.status === 401 || rejection.status === 400) {
+          var MsgService = $injector.get('MsgService');
+          MsgService.error(rejection.data);          
         }
         return rejection;
       }
